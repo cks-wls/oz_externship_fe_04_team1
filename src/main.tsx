f5b727f3f87f544e4795e9d259529183f80e10bd
cks-wls <slo0521@naver.com>
2025-11-21 19:40:05 +0900

    build : 전역 스타일링 추가 및 tailwind css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
-
+import '@/index.css'
 // MSW 워커를 활성화하는 함수(개발환경에서만 실행)
 async function enableMocking() {
   if (import.meta.env.MODE !== 'development') {
